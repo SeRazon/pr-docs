1cb49a91ba8cfe063a9a1d3571e62b2c0352b3aa
星鸿SeRazon <weizixuan110@foxmail.com>
2025-10-17 23:33:25 +0800

    add:  Internet Content Provider filing

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,157 +1,97 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
-    lang: 'zh-cn',
-    title: 'Phi Recorder',
-    description: '你的下一个渲染器',
-    lastUpdated: true,
+  lang: 'zh-cn',
+  title: 'Phi Recorder',
+  description: '你的下一个渲染器',
+  lastUpdated: true,
 
-    head: [
-        ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }],
-        ['link', { rel: 'manifest', href: '/site.webmanifest' }],
-        ['link', { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' }],
-        ['meta', { name: 'msapplication-TileColor', content: '#00aba9' }],
-        ['meta', { name: 'theme-color', content: '#ffffff' }],
+  head: [
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }],
+    ['link', { rel: 'manifest', href: '/site.webmanifest' }],
+    ['link', { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#00aba9' }],
+    ['meta', { name: 'theme-color', content: '#ffffff' }],
+  ],
+
+  markdown: {
+    lineNumbers: true,
+    theme: 'one-dark-pro'
+  },
+
+  cleanUrls: true,
+
+  themeConfig: {
+    logo: '/logo.png',
+
+    footer: {
+      message: [
+        'Documentation released under the MIT License, all rights reserved.',
+        `<br>`,
+        `<a href="https://beian.miit.gov.cn/" target="_blank" rel="noopener noreferrer">赣ICP备2025074193号</a>`,
+        `<span style="margin: 0 8px; color: #999">|</span>`,
+        `<a href="https://beian.mps.gov.cn/#/query/webSearch?code=36012202000590" target="_blank" rel="noopener noreferrer">`,
+        `<img src="https://www.beian.gov.cn/img/ghs.png" alt="公安备案图标" style="width:16px;height:16px;margin-right:4px;vertical-align:-3px;">`,
+        `赣公网安备36012202000590号</a>`
+      ].join(''),
+      copyright: 'Copyright © 2025 - Phi Recorder'
+    },
+
+    sidebar: {
+      '/guide/': sidebarGuide(),
+      '/config/': sidebarGuide(),
+      '/writing/': sidebarGuide(),
+    },
+
+    nav: [
+      { text: '指南', link: '/guide/' },
     ],
 
-    markdown: {
-        lineNumbers: true,
-        theme: 'one-dark-pro'
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/2278535805/Phi-Recorder' }
+    ],
+
+    editLink: {
+      pattern: 'https://github.com/SeRazon/pr-docs/edit/master/docs/:path',
+      text: '在 GitHub 上编辑此页面'
     },
 
-    cleanUrls: true,
+    outline: [2, 3],
 
-    themeConfig: {
-        logo: '/logo.png',
-
-        footer: {
-            message: "Documentation released under the MIT License, all rights reserved.",
-            copyright: "Copyright © 2025 - Phi Recorder",
-        },
-
-        sidebar: {
-            '/guide/': sidebarGuide(),
-            '/config/': sidebarGuide(),
-            '/writing/': sidebarGuide(),
-        },
-
-        nav: [
-            { text: '指南', link: '/guide/' },
-            //{ text: 'Config', link: '/config/' },
-        ],
-
-        socialLinks: [
-            { icon: 'github', link: 'https://github.com/2278535805/Phi-Recorder' }
-        ],
-
-        editLink: {
-            pattern: 'https://github.com/SeRazon/pr-docs/edit/master/docs/:path',
-            text: '在 GitHub 上编辑此页面'
-        },
-
-        outline: [2, 3],
-
-        algolia: {
-            appId: '6OC1XCG4R5',
-            apiKey: '7779946cc768ec3699123e60a91d0ddc',
-            indexName: 'stack-jimmycai',
-        }
-    },
+    algolia: {
+      appId: '6OC1XCG4R5',
+      apiKey: '7779946cc768ec3699123e60a91d0ddc',
+      indexName: 'stack-jimmycai',
+    }
+  },
 });
 
 function sidebarGuide() {
-    return [
-        {
-            text: '介绍',
-            collapsible: true,
-            items: [
-                { text: '关于 Phi Recorder', link: '/guide/' },
-                { text: '快速开始', link: '/guide/getting-started' },
-               // { text: 'Modify Theme', link: '/guide/modify-theme' }
-            ]
-        },
-        {
-            text: '基础',
-            collapsible: true,
-            items: [
-                { text: '谱面信息', link: '/guide/base/chart-info' },
-                { text: '渲染设置', link: '/guide/base/render-config' },
-                { text: 'RPE 绑定', link: '/guide/base/rpe-bind' },
-            ]
-        },
-        {
-            text: '接口',
-            collapsible: true,
-            items: [
-                { text: '参数', link: '/guide/connect/args' },
-                { text: '资源包', link: '/guide/connect/res-pack' },
-            ]
-        },
-        /*{
-            text: 'Config',
-            collapsible: true,
-            items: [
-                {
-                    text: 'Introduction',
-                    link: '/config/'
-                },
-                {
-                    text: 'Site Configs',
-                    link: '/config/site'
-                },
-                {
-                    text: 'i18n Configs',
-                    link: '/config/i18n'
-                },
-                {
-                    text: 'Custom Menu',
-                    link: '/config/menu'
-                },
-                {
-                    text: 'Custom Header / Footer',
-                    link: '/config/header-footer'
-                },
-                {
-                    text: 'Date Format',
-                    link: '/config/date-format'
-                },
-                {
-                    text: 'Sidebar',
-                    link: '/config/sidebar'
-                },
-                {
-                    text: 'Footer',
-                    link: '/config/footer'
-                },
-                {
-                    text: 'Article',
-                    link: '/config/article'
-                },
-                {
-                    text: 'Comments',
-                    link: '/config/comments'
-                },
-                {
-                    text: 'Widgets',
-                    link: '/config/widgets'
-                },
-                {
-                    text: 'Open Graph',
-                    link: '/config/open-graph'
-                },
-                {
-                    text: 'Default Image',
-                    link: '/config/default-image'
-                },
-                {
-                    text: 'Color Scheme',
-                    link: '/config/color-scheme'
-                },
-                {
-                    text: 'Image Processing',
-                    link: '/config/image-processing'
-                }
-            ]
-        },*/
-    ]
+  return [
+    {
+      text: '介绍',
+      collapsible: true,
+      items: [
+        { text: '关于 Phi Recorder', link: '/guide/' },
+        { text: '快速开始', link: '/guide/getting-started' },
+      ]
+    },
+    {
+      text: '基础',
+      collapsible: true,
+      items: [
+        { text: '谱面信息', link: '/guide/base/chart-info' },
+        { text: '渲染设置', link: '/guide/base/render-config' },
+        { text: 'RPE 绑定', link: '/guide/base/rpe-bind' },
+      ]
+    },
+    {
+      text: '接口',
+      collapsible: true,
+      items: [
+        { text: '参数', link: '/guide/connect/args' },
+        { text: '资源包', link: '/guide/connect/res-pack' },
+      ]
+    },
+  ]
 }
